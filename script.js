ea6de68265c70df18b9fd89486fa74ac93a2eac6
kauani <kauani.binda@escola.pr.gov.br>
2025-10-03 11:50:46 -0300

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelectorA11('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventlistener('click', function(){
+       const atual = documnt.queryselector('.ativo');
+       const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+       atual.classList.remove('ativo');
+       document.getElementById(proximopasso).classList.add('ativo'):
+    })
+})
